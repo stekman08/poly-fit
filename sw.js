93e8d5cb2e60eeef2acca96010fd767baae40c65
stekman08 <stekman08@users.noreply.github.com>
2025-12-26 17:11:17 +0000

    deploy: 32088cf982ad33eb1b44f19357b1467c0edf92d8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Version: 4abd21e • 2025-12-26 17:05
+// Version: 32088cf • 2025-12-26 18:08
 // PolyFit Service Worker
 // Cache name will be set dynamically based on version
 let CACHE_NAME = 'polyfit-v1'; // fallback
